0b1bc7d1ba6c4201fd17bada2bc0ecf671d96da9
Lucas Teixeira <89080606+teixeiralucs@users.noreply.github.com>
2025-11-06 21:11:49 -0300

    Entrega da Atividade 8 com excessão do exercicio 4

---
+++ ATIVIDADE_8/Exercise_2.java
@@ -0,0 +1,22 @@
+package ATIVIDADE_8;
+
+public class Exercise_2 {
+    //2
+    public abstract class top{}
+    public class tip extends top{}
+
+    //3
+    public abstract class Fee{}
+    public abstract class Fi extends Fee{}
+
+    //4 
+    public class Foo{}
+    public class Bar implements Foo{}
+    public class Baz extends Bar{}
+
+    //5
+    public class Zeta{}
+    public class Beta{}
+    public class Alpha implements Zeta{}
+    public class Delta extends Alpha implements Beta{}
+}
